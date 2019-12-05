f4efd2e7ee4fb4f88a01fc9a3bc1914668038afa
Viktor Pyskunov <vicpi89@gmail.com>
2019-12-05 17:26:48 +0200

    Add Map Sidebar

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Map.scss';
+
+const Map: React.FC = () => {
+    return (
+        <div className="map-container">
+            Map
+        </div>
+    );
+}
+    
+export default Map;
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.scss';
+
+const Sidebar: React.FC = () => {
+    return (
+        <div className="sidebar-container">
+            Sidebar
+        </div>
+    );
+}
+    
+export default Sidebar;

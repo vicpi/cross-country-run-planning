a573d43574d7d727720b9f6e9ab772d7f4c83de8
Viktor Pyskunov <vicpi89@gmail.com>
2019-12-06 17:25:50 +0200

    Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header: React.FC = () => {
+    return (
+        <div className="header">
+            <p>Route Builder</p>
+        </div>
+    );
+}
+
+export default Header;

98324aa145a8f43185b8ea4eee01d98d155f6688
Viktor Pyskunov <vicpi89@gmail.com>
2019-12-06 17:26:06 +0200

    Add List component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './List.scss';
+
+const List: React.FC = () => {
+    return (
+        <div className="list">
+            <p>Item</p>
+            <p>Item</p>
+        </div>
+    );
+}
+
+export default List;

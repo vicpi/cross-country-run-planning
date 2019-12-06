93b63d569e5f28a8c190b3e17dc2c0835749fd71
Viktor Pyskunov <vicpi89@gmail.com>
2019-12-06 17:27:06 +0200

    Improve Sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from '../Header/Header';
+import List from '../List/List';
+import Footer from '../Footer/Footer';
 import './Sidebar.scss';
 
 const Sidebar: React.FC = () => {
     return (
         <div className="sidebar-container">
-            Sidebar
+            <Header />
+            <List />
+            <Footer />
         </div>
     );
 }
